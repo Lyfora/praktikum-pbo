d64e63f50bf7e5ea556ba5ff890a4e38e7ceaee5
Axelliano Rafael S <100137021+Lyfora@users.noreply.github.com>
2023-05-10 16:10:12 +0700

    Update and rename PolimorfismeCoercion to PolimorfismeCoercion.java

--- pertemuan7/PolimorfismeCoercion.java
+++ pertemuan7/PolimorfismeCoercion.java
@@ -1,3 +1,6 @@
+//        File : PolimorfismeCoercion.java  09/05/2023
+//        Penulias : Axelliano Rafael Situmeang / 24060121130089
+//        Deskripsi : driver main untuk Pegawai,manajer,programmer, dan Payroll
 public class PolimorfismeCoercion {
     public static void main(String[] args){
          Pegawai pegawai = new Programmer("Mira");

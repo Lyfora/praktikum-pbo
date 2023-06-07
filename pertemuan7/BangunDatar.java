8e9461bb186c164376f014f38dea3bd02e7a291a
Axelliano Rafael S <100137021+Lyfora@users.noreply.github.com>
2023-06-07 11:01:19 +0700

    Create BangunDatar.java

---
+++ pertemuan7/BangunDatar.java
@@ -0,0 +1,6 @@
+//        File : BangunDatar.java  
+//        Penulias : Axelliano Rafael Situmeang / 24060121130089
+//        Deskripsi : abstract class untuk BangunDatar
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}

eb4c0472103ea5905013378babbfcc0237662b30
Axelliano Rafael S <100137021+Lyfora@users.noreply.github.com>
2023-06-07 10:17:03 +0700

    Create Poligon.java

---
+++ pertemuan4/Poligon.java
@@ -0,0 +1,14 @@
+//        File : Poligon.java  08/03/2023
+//        Penulias : Axelliano Rafael Situmeang / 24060121130089
+//        Deskripsi : class dasar untuk berbagai bentuk 2D
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public void setJumlahSisi(int sisi){
+        jumlahSisi = sisi;
+    }
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    } 
+}
